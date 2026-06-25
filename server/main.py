1b2346f191de9120fb0df7634701cf3b344b1333
d13l69 <demalesha6969@gmail.com>
2026-06-25 12:06:26 +0700

    тестовый запрос

--- server/main.py
+++ server/main.py
@@ -1,11 +1,19 @@
-from fastapi import APIRouter
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from server.response import ApiResponse
 
-router = APIRouter()
 
+app = FastAPI()
 
-@router.get("/")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
 def root():
 
     return ApiResponse.success(
