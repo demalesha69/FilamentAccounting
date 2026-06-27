1993e95b36bbd65f53acb5313f4affdf89e61336
d13l69 <demalesha6969@gmail.com>
2026-06-27 19:20:30 +0700

    Редизайн кода

--- server/exceptions/exception_handlers.py
+++ server/exceptions/exception_handlers.py
@@ -4,7 +4,7 @@ from server.exceptions.materialExceptions import MaterialException
 from server.exceptions.authExceptions import AuthException
 from server.exceptions.consumptionExceptions import ConsumptionException
 
-from server.response import ApiResponse, JSONResponse
+from server.utils.response import ApiResponse
 
 def material_exception_handler(request: Request, exc: MaterialException):
     return ApiResponse.error(
