4850087d163d433997ee8c329d4d47949c774c46
d13l69 <demalesha6969@gmail.com>
2026-06-29 22:57:28 +0700

    Обновление модели

--- database/models/material.py
+++ database/models/material.py
@@ -27,9 +27,12 @@ class Material(Base):
         String(50)
     )
 
-    density: Mapped[float | None] = mapped_column(
-        Float,
-        nullable=True
+    density: Mapped[float] = mapped_column(
+        Float
+    )
+
+    diameter: Mapped[float] = mapped_column(
+        Float
     )
 
     color: Mapped[str] = mapped_column(
